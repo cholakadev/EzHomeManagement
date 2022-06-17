6638169e9ccf3ea186b8191b815bc57b8d159f45
gecho <cholakovge@gmail.com>
2022-06-17 18:31:45 +0300

    New component which handles the rendering of all the news

--- Client/ez-hm/src/App.js
+++ Client/ez-hm/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Container from './components/Layout/Container';
 import Header from './components/Layout/Header';
-import New from './components/News/New';
+import LastestNews from './components/News/LatestNews';
 import Card from './components/UI/Card';
 
 const DUMMY_DATA = [
@@ -21,22 +21,26 @@ const DUMMY_DATA = [
     author: 'Georgi Cholakov',
     apartment: '10A',
     date: new Date(2022, 7, 20)
+  },  
+  {
+    id: 3,
+    title: 'Free parkspace',
+    content: 'Since we will be out of the town for 20 days, our parking space is free and can be used until 21/09/2022 22:00 PM.',
+    author: 'Georgi Cholakov',
+    apartment: '10A',
+    date: new Date(2022, 7, 20)
+  },
+  {
+    id: 2,
+    title: 'Free parkspace',
+    content: 'Since we will be out of the town for 20 days, our parking space is free and can be used until 21/09/2022 22:00 PM.',
+    author: 'Georgi Cholakov',
+    apartment: '10A',
+    date: new Date(2022, 7, 20)
   },
 ];
 
 function App() {
-
-  var lastestNews = DUMMY_DATA.map(lastestNew => 
-    <li key={lastestNew.id}>
-      <New
-        title={lastestNew.title} 
-        date={lastestNew.date} 
-        content={lastestNew.content}
-        author={lastestNew.author}
-        apartment={lastestNew.apartment}
-      />
-    </li>);
-
   return (
     <React.Fragment>
       <Header />
@@ -44,9 +48,7 @@ function App() {
         <div className='inner-container'>
           <div className='side-section'>
             <Card>
-              <ul className='side-section-items'>
-                {lastestNews}
-              </ul>
+              <LastestNews latestNews={DUMMY_DATA} />
             </Card>
           </div>
         </div>
