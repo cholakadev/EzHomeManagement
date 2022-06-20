1932473a768c9f136bb108b72441514eee9feabd
gecho <cholakovge@gmail.com>
2022-06-20 20:58:51 +0300

    Controller endpoint for registering entrance

--- Server/EzHomeManagement/EzHomeManagement.API/Controllers/AuthController.cs
+++ Server/EzHomeManagement/EzHomeManagement.API/Controllers/AuthController.cs
@@ -1,9 +1,25 @@
 ﻿namespace EzHomeManagement.API.Controllers
 {
+    using EzHomeManagement.Business.Abstraction;
+    using EzHomeManagement.Business.Models.Auth.Entrance;
+    using FluentResult;
     using Microsoft.AspNetCore.Mvc;
 
+    [ApiController]
+    [Route("api/[controller]")]
     public class AuthController : Controller
     {
-        
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            this._authService = authService;
+        }
+
+        [HttpPost]
+        [Route("RegisterEntrance")]
+        [ProducesResponseType(typeof(Result<string>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public Task<IActionResult> RegisterEntrance([FromBody] RegisterEntranceModel entrance)
+            => this._authService.RegisterEntrance(entrance).ToActionResultAsync(this);
     }
 }
