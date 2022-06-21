5f35b835fc00e8290d7b0c27c81da556739b5ebb
gecho <cholakovge@gmail.com>
2022-06-21 11:16:59 +0300

    User DTOs

---
+++ Server/EzHomeManagement/EzHomeManagement.Business.Models/Auth/User/AuthModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EzHomeManagement.Business.Models.Auth.User
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AuthModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Server/EzHomeManagement/EzHomeManagement.Business.Models/Auth/User/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EzHomeManagement.Business.Models.Auth.User
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+    }
+}
