5767203b3ace08f0fa67b300e0604b828c99ba8c
gecho <cholakovge@gmail.com>
2022-06-21 11:17:30 +0300

    Factory extension method to convert entity to model

---
+++ Server/EzHomeManagement/EzHomeManagement.Business/Factories/UserFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace EzHomeManagement.Business.Factories
+{
+    using EzHomeManagement.Business.Models.Auth.User;
+    using EzHomeManagement.Data.Models.EntityModels;
+
+    public static class UserFactory
+    {
+        public static UserModel ToModel(this User user)
+            => new UserModel
+            {
+                Id = user.Id,
+                Email = user.Email
+            };
+    }
+}
